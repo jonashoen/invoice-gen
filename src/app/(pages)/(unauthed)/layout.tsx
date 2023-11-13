df7595dbd6964a124e05b85159d5a7c6f46fe2ce
jonashoen <hoenisch.jonas@gmail.com>
2023-11-13 21:24:14 +0100

    removed version from unathed screens

--- src/app/(pages)/(unauthed)/layout.tsx
+++ src/app/(pages)/(unauthed)/layout.tsx
@@ -14,11 +14,8 @@ const Layout = async ({ children }: { children: ReactNode }) => {
   return (
     <main className="mx-auto min-h-screen flex flex-col justify-center">
       <div className="container flex flex-col mx-auto gap-20">
-        <Container className="bg-green mt-20">
-          <p className="font-bold py-4 text-6xl flex justify-between">
-            <span>invoice-gen</span>
-            <span>v1.2.0</span>
-          </p>
+        <Container className="bg-green mt-20 w-fit">
+          <p className="font-bold py-4 text-6xl">invoice-gen</p>
         </Container>
         {children}
       </div>
