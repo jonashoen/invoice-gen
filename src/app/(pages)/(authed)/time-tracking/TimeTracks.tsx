e1be28dc5ba786382114971413cd55702afb3c69
jonashoen <hoenisch.jonas@gmail.com>
2024-02-13 18:39:13 +0100

    group timetracks by project for each day

--- src/app/(pages)/(authed)/time-tracking/TimeTracks.tsx
+++ src/app/(pages)/(authed)/time-tracking/TimeTracks.tsx
@@ -7,6 +7,7 @@ import LocalTime from "@/components/LocalTime";
 import Pagination from "@/components/Pagination";
 import Paper from "@/components/Paper";
 import getDurationString from "@/helper/getDurationString";
+import group from "@/helper/groupArray";
 import AddTimeTracking from "@/modals/AddTimeTracking";
 import { TimeTrack, TimeTrackActivity } from "@prisma/client";
 import dayjs from "dayjs";
@@ -38,75 +39,105 @@ const TimeTracks: React.FC<Props> = ({ groupedTimeTracks, isTracking }) => {
               0
             );
 
-            const virtualTrackingStarted = dayjs
-              .utc()
-              .subtract(millisecondsTracked, "milliseconds")
-              .toDate();
-
             const trackedTodayString =
-              getDurationString(virtualTrackingStarted) || "0h";
+              getDurationString(millisecondsTracked) || "0h";
+
+            const groupedTimeTracksByProjectAndDate = group(
+              groupedTimeTracks[day],
+              { key: "projectId" }
+            );
 
             return (
               <Details title={`${day} (${trackedTodayString})`} key={day}>
                 <div className="flex flex-col gap-4">
-                  {groupedTimeTracks[day].map((timeTrack) => (
-                    <Paper key={timeTrack.id}>
-                      <div className="flex justify-between items-center border-black border-b pb-[12px]">
-                        <p className="flex-1 text-xl">
-                          {timeTrack.project.name} (
-                          {timeTrack.project.customer.name})
-                        </p>
+                  {Object.keys(groupedTimeTracksByProjectAndDate).map(
+                    (projectId) => {
+                      const baseTimeTrack =
+                        groupedTimeTracksByProjectAndDate[projectId][0];
 
-                        <div className="flex gap-4">
-                          <LocalTime date={timeTrack.startTime} />
-                          <p>-</p>
-                          <LocalTime date={timeTrack.endTime!} />
-                        </div>
-                        <div className="flex flex-1 items-center justify-end gap-10">
-                          <p className="text-xl">
-                            {getDurationString(
-                              timeTrack.startTime,
-                              timeTrack.endTime
-                            ) || "0h"}
-                          </p>
+                      const project = baseTimeTrack.project;
 
-                          {!timeTrack.project.archived && (
-                            <div className="flex gap-2">
-                              <ModalButton
-                                modal={{
-                                  title: "Zeiterfassung bearbeiten",
-                                  content: (
-                                    <AddTimeTracking
-                                      timeTrackId={timeTrack.id}
-                                      oldProjectId={timeTrack.projectId}
-                                      oldStartTime={
-                                        new Date(timeTrack.startTime)
-                                      }
-                                      oldEndTime={new Date(timeTrack.endTime!)}
-                                      oldActivities={timeTrack.activities}
-                                    />
-                                  ),
-                                }}
-                              >
-                                Bearbeiten
-                              </ModalButton>
+                      return (
+                        <Paper key={day + projectId} className="!py-6">
+                          <div className="flex justify-between items-center border-black border-b pb-4">
+                            <p className="flex-1 text-xl">
+                              {project.name} ({project.customer.name})
+                            </p>
 
+                            {!project.archived && (
                               <RestartTimeTrackingButton
-                                projectId={timeTrack.projectId}
+                                projectId={baseTimeTrack.projectId}
                                 disabled={isTracking}
                               />
-                            </div>
-                          )}
-                        </div>
-                      </div>
+                            )}
+                          </div>
 
-                      <ul className="mt-2 ml-4 list-['-_']">
-                        {timeTrack.activities.map((activity) => (
-                          <li key={activity.id}>{activity.description}</li>
-                        ))}
-                      </ul>
-                    </Paper>
-                  ))}
+                          {groupedTimeTracksByProjectAndDate[projectId].map(
+                            (timeTrack) => (
+                              <div
+                                key={timeTrack.id}
+                                className="flex bg-gray-100 items-start gap-4 p-4 py-4 mt-4 rounded-lg"
+                              >
+                                <Paper className="flex-1">
+                                  <ul className="pl-8 list-['-_']">
+                                    {timeTrack.activities.map((activity) => (
+                                      <li key={activity.id}>
+                                        {activity.description}
+                                      </li>
+                                    ))}
+                                  </ul>
+                                </Paper>
+
+                                <div className="flex items-center justify-between flex-1 gap-10">
+                                  <div className="flex gap-4 text-lg items-center ml-12">
+                                    <LocalTime date={timeTrack.startTime} />
+                                    <p>-</p>
+                                    <LocalTime date={timeTrack.endTime!} />
+                                    <p className="text-base">
+                                      (
+                                      {getDurationString(
+                                        timeTrack.startTime,
+                                        timeTrack.endTime
+                                      ) || "0h"}
+                                      )
+                                    </p>
+                                  </div>
+
+                                  {!timeTrack.project.archived && (
+                                    <div className="flex gap-2">
+                                      <ModalButton
+                                        className="bg-white"
+                                        modal={{
+                                          title: "Zeiterfassung bearbeiten",
+                                          content: (
+                                            <AddTimeTracking
+                                              timeTrackId={timeTrack.id}
+                                              oldProjectId={timeTrack.projectId}
+                                              oldStartTime={
+                                                new Date(timeTrack.startTime)
+                                              }
+                                              oldEndTime={
+                                                new Date(timeTrack.endTime!)
+                                              }
+                                              oldActivities={
+                                                timeTrack.activities
+                                              }
+                                            />
+                                          ),
+                                        }}
+                                      >
+                                        Bearbeiten
+                                      </ModalButton>
+                                    </div>
+                                  )}
+                                </div>
+                              </div>
+                            )
+                          )}
+                        </Paper>
+                      );
+                    }
+                  )}
                 </div>
               </Details>
             );
